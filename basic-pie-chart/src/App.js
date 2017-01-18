1a5b88780445389f69c7a805105a3cbddce142eb
Jonathon Kelly <jonathon@insidersbyte.com>
2017-01-18 23:46:29 +0000

    :art: simplify the styling of the pie charts

--- basic-pie-chart/src/App.js
+++ basic-pie-chart/src/App.js
@@ -1,20 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import PieChart from './PieChart';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-
-        <div className="App-intro">
-          <PieChart />
-        </div>
+        <PieChart />
       </div>
     );
   }
--- pie-chart/src/App.js
+++ pie-chart/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import PieChart from './PieChart';
+import './App.css';
 
 class App extends Component {
   state = {
@@ -22,12 +21,7 @@ class App extends Component {
     const { data, width, height } = this.state;
 
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-
+    <div className="App">
         <PieChart
           data={data}
           width={width}
