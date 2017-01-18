2550a098151d43f0258ce0cf3a78575af7dbd5c2
Jonathon Kelly <jonathon@insidersbyte.com>
2017-01-18 21:22:07 +0000

    :sparkles: basic pie chart

---
+++ README.md
@@ -0,0 +1,5 @@
+# Charts in React
+
+These are the code examples from my talk on Charts in React with D3.
+
+* View the slides: http://bit.ly/2j1rcNZ
---
+++ basic-pie-chart/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PieChart from './PieChart';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <div className="App-intro">
+          <PieChart />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
