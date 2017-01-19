3f9c4f72aea2b737a352276ba4c88e6ab5ec3c3b
Jonathon Kelly <jonathon@insidersbyte.com>
2017-01-19 00:00:35 +0000

    :sparkles: responsive pie chart

---
+++ response-pie-chart/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Dimensions from 'react-dimensions';
+import PieChart from './PieChart';
+import './App.css';
+
+class App extends Component {
+  state = {
+    data: [
+      { label: '<5', value: 2704659 },
+      { label: '5-13', value: 4499890 },
+      { label: '14-17', value: 2159981 },
+      { label: '18-24', value: 3853788 },
+      { label: '25-44', value: 14106543 },
+      { label: '45-64', value: 8819342 },
+      { label: '≥65', value: 612463 },
+    ],
+  };
+
+  render() {
+    const { data } = this.state;
+    const { containerWidth, containerHeight } = this.props;
+
+    return (
+    <div className="App">
+        <PieChart
+          data={data}
+          width={containerWidth}
+          height={containerHeight}
+        />
+      </div>
+    );
+  }
+}
+
+export default Dimensions()(App);
---
+++ response-pie-chart/src/PieChart.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { pie as Pie, arc } from 'd3-shape';
+import { scaleOrdinal } from 'd3-scale';
+import './PieChart.css';
+
+const size = 500;
+const radius = size / 2;
+
+const dataArc = arc()
+  .outerRadius(radius - 10)
+  .innerRadius(0);
+
+const labelArc = arc()
+  .outerRadius(radius - 40)
+  .innerRadius(radius - 40);
+
+const colour = scaleOrdinal(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+const pie = new Pie()
+  .sort(null)
+  .value(d => d.value);
+
+const PieChart = ({ data, width, height }) => {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox={`0 0 ${size} ${size}`}
+    >
+      <g transform={`translate(${radius}, ${radius})`}>
+        {pie(data).map((d, i) => (
+          <g key={i} className="arc">
+            <path d={dataArc(d)} fill={colour(d.data.label)} />
+
+            <text dy=".35em" transform={`translate(${labelArc.centroid(d)})`}>{d.data.label}</text>
+          </g>
+        ))}
+      </g>
+    </svg>
+  );
+};
+
+export default PieChart;
